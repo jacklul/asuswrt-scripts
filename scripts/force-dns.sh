a4231c75c8d4e1f2fdcab6048a033d524f85fdd3
Jack'lul <jacklulcat@gmail.com>
2023-04-20 22:44:10 +0200

    Add option to block router's DNS server when rules are set

--- scripts/force-dns.sh
+++ scripts/force-dns.sh
@@ -19,6 +19,7 @@ REQUIRE_INTERFACE="" # rules will be removed if this interface does not exist in
 FALLBACK_DNS_SERVER="" # set to this DNS server when interface defined in REQUIRE_INTERFACE does not exist
 FALLBACK_DNS_SERVER6="" # set to this DNS server (IPv6) when interface defined in REQUIRE_INTERFACE does not exist
 EXECUTE_COMMAND="" # Execute a command after rules are applied or removed, will pass argument with action (add or remove)
+BLOCK_ROUTER_DNS=false # block access to router's DNS server while the rules are set, to be used with REQUIRE_INTERFACE and "Advertise router as DNS" option
 CRON_MINUTE="*/1"
 CRON_HOUR="*"
 
@@ -27,6 +28,7 @@ IPT="/usr/sbin/iptables"
 IPT6="/usr/sbin/ip6tables"
 CHAIN="FORCEDNS"
 CHAIN_DOT="FORCEDNS_DOT"
+CHAIN_BLOCK="FORCEDNS_BLOCK"
 
 # If you need per-device DNS settings then these will help you write your own script (which you can execute via EXECUTE_COMMAND variable):
 #iptables -I "FORCEDNS" -m mac --mac-source "d9:32:cb:d0:fe:fe" -j DNAT --to-destination "1.1.1.1"
@@ -46,6 +48,7 @@ fi
 [ ! -f "$IPT" ] && { echo "Missing iptables binary: $IPT"; exit 1; }
 
 FOR_IPTABLES="$IPT"
+ROUTER_IP="$(nvram get lan_ipaddr)"
 
 if [ -z "$DNS_SERVER" ]; then
     DHCP_DNS1="$(nvram get dhcp_dns1_x)"
@@ -53,7 +56,7 @@ if [ -z "$DNS_SERVER" ]; then
     if [ -n "$DHCP_DNS1" ]; then
         DNS_SERVER="$DHCP_DNS1"
     else
-        DNS_SERVER="$(nvram get lan_ipaddr)"
+        DNS_SERVER="$ROUTER_IP"
     fi
 fi
 
@@ -69,23 +72,34 @@ if [ "$(nvram get ipv6_service)" != "disabled" ]; then
     fi
 fi
 
+{ [ "$BLOCK_ROUTER_DNS" = "true" ] || [ "$BLOCK_ROUTER_DNS" = true ]; } && BLOCK_ROUTER_DNS="1" || BLOCK_ROUTER_DNS="0"
+
 # These "iptables_" functions are based on code from YazFi (https://github.com/jackyaz/YazFi) then modified using code from dnsfiler.c
 iptables_chains() {
     for _IPTABLES in $FOR_IPTABLES; do
         case "$1" in
             "add")
-                _FWRDSTART="$($_IPTABLES -nvL FORWARD --line | grep -E "all.*state RELATED,ESTABLISHED" | tail -1 | awk '{print $1}')"
-                _FWRDSTART_PLUS="$((_FWRDSTART+1))"
-
                 if ! $_IPTABLES -n -L "$CHAIN_DOT" >/dev/null 2>&1; then
+                    _FORWARD_START="$($_IPTABLES -nvL FORWARD --line | grep -E "all.*state RELATED,ESTABLISHED" | tail -1 | awk '{print $1}')"
+                    _FORWARD_START_PLUS="$((_FORWARD_START+1))"
+                    
                     $_IPTABLES -N "$CHAIN_DOT"
-                    $_IPTABLES -I FORWARD "$_FWRDSTART_PLUS" -i "$BRIDGE_INTERFACE" -p tcp -m tcp --dport 853 -j "$CHAIN_DOT"
+                    $_IPTABLES -I FORWARD "$_FORWARD_START_PLUS" -i "$BRIDGE_INTERFACE" -p tcp -m tcp --dport 853 -j "$CHAIN_DOT"
                 fi
 
                 if ! $_IPTABLES -t nat -n -L "$CHAIN" >/dev/null 2>&1; then
                     $_IPTABLES -t nat -N "$CHAIN"
-                    $_IPTABLES -t nat -I PREROUTING -i "$BRIDGE_INTERFACE" -p udp -m udp --dport 53 -j "$CHAIN"
                     $_IPTABLES -t nat -I PREROUTING -i "$BRIDGE_INTERFACE" -p tcp -m tcp --dport 53 -j "$CHAIN"
+                    $_IPTABLES -t nat -I PREROUTING -i "$BRIDGE_INTERFACE" -p udp -m udp --dport 53 -j "$CHAIN"
+                fi
+
+                if [ "$BLOCK_ROUTER_DNS" = "1" ] && ! $_IPTABLES -n -L "$CHAIN_BLOCK" >/dev/null 2>&1; then
+                    _INPUT_START="$($_IPTABLES -nvL INPUT --line | grep -E "all.*state INVALID" | tail -1 | awk '{print $1}')"
+                    _INPUT_START_PLUS="$((_INPUT_START+1))"
+
+                    $_IPTABLES -N "$CHAIN_BLOCK"
+                    $_IPTABLES -I INPUT "$_INPUT_START_PLUS" -i "$BRIDGE_INTERFACE" -p tcp -m tcp --dport 53 -d "$ROUTER_IP" -j "$CHAIN_BLOCK"
+                    $_IPTABLES -I INPUT "$_INPUT_START_PLUS" -i "$BRIDGE_INTERFACE" -p udp -m udp --dport 53 -d "$ROUTER_IP" -j "$CHAIN_BLOCK"
                 fi
             ;;
             "remove")
@@ -100,6 +114,13 @@ iptables_chains() {
                     $_IPTABLES -t nat -D PREROUTING -i "$BRIDGE_INTERFACE" -p tcp -m tcp --dport 53 -j "$CHAIN"
                     $_IPTABLES -t nat -F "$CHAIN"
                     $_IPTABLES -t nat -X "$CHAIN"
+                fi
+
+                if $_IPTABLES -n -L "$CHAIN_BLOCK" >/dev/null 2>&1; then
+                    $_IPTABLES -D INPUT -i "$BRIDGE_INTERFACE" -p udp -m udp --dport 53 -d "$ROUTER_IP" -j "$CHAIN_BLOCK"
+                    $_IPTABLES -D INPUT -i "$BRIDGE_INTERFACE" -p tcp -m tcp --dport 53 -d "$ROUTER_IP" -j "$CHAIN_BLOCK"
+                    $_IPTABLES -F "$CHAIN_BLOCK"
+                    $_IPTABLES -X "$CHAIN_BLOCK"
                 fi
             ;;
         esac
@@ -142,6 +163,7 @@ iptables_rules() {
 
                 $_IPTABLES -t nat "$_ACTION" "$CHAIN" -m mac --mac-source "$MAC" -j RETURN
                 $_IPTABLES "$_ACTION" "$CHAIN_DOT" -m mac --mac-source "$MAC" -j RETURN
+                $_IPTABLES "$_ACTION" "$CHAIN_BLOCK" -m mac --mac-source "$MAC" -j RETURN
             done
         fi
 
@@ -152,18 +174,29 @@ iptables_rules() {
                 if [ "${IP#*"-"}" != "$IP" ]; then # IP range entry
                     $_IPTABLES -t nat "$_ACTION" "$CHAIN" -m iprange --src-range "$IP" -j RETURN
                     $_IPTABLES "$_ACTION" "$CHAIN_DOT" -m iprange --src-range "$IP" -j RETURN
+                    $_IPTABLES "$_ACTION" "$CHAIN_BLOCK" -m iprange --src-range "$IP" -j RETURN
                 else # single IP entry
                     $_IPTABLES -t nat "$_ACTION" "$CHAIN" -s "$IP" -j RETURN
                     $_IPTABLES "$_ACTION" "$CHAIN_DOT" -s "$IP" -j RETURN
+                    $_IPTABLES "$_ACTION" "$CHAIN_BLOCK" -s "$IP" -j RETURN
                 fi
             done
         else # no IP ranges found, conveniently iptables accept IPs separated by commas
             $_IPTABLES -t nat "$_ACTION" "$CHAIN" -s "$_PERMIT_IP" -j RETURN
             $_IPTABLES "$_ACTION" "$CHAIN_DOT" -s "$_PERMIT_IP" -j RETURN
+            $_IPTABLES "$_ACTION" "$CHAIN_BLOCK" -s "$_PERMIT_IP" -j RETURN
         fi
 
+        if [ "$BLOCK_ROUTER_DNS" = "1" ]; then
+            $_IPTABLES -t nat "$_ACTION" "$CHAIN" -d "$ROUTER_IP" -j RETURN
+        fi
+        
         $_IPTABLES -t nat "$_ACTION" "$CHAIN" -j DNAT --to-destination "$_SET_DNS_SERVER"
         $_IPTABLES "$_ACTION" "$CHAIN_DOT" ! -d "$_SET_DNS_SERVER" -j REJECT
+
+        if [ "$BLOCK_ROUTER_DNS" = "1" ]; then
+            $_IPTABLES "$_ACTION" "$CHAIN_BLOCK" -j REJECT
+        fi
     done
 }
 
