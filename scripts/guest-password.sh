74d7d0c59edc40b65fe4d5b94c1dd5b2ca6aaa1e
Jack'lul <jacklulcat@gmail.com>
2023-10-27 23:02:18 +0200

    Remove HTML generation as functionality implemented in modify-webui script

--- scripts/guest-password.sh
+++ scripts/guest-password.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 # Made by Jack'lul <jacklul.github.io>
 #
-# Randomize Guest WiFi passwords and generate HTML pages for them
-#
-# Pages will be available at www.asusrouter.com/user/guest-INTERFACE.html and www.asusrouter.com/user/guest-list.html
+# Randomize Guest WiFi passwords
 #
 # Based on:
 #  https://github.com/RMerl/asuswrt-merlin.ng/wiki/Guest-WIFI-QR-code-generator-for-display-on-local-network-webpage-(visible-from-TV,-smartphones...)-and-random-password-rotation
@@ -17,12 +15,10 @@ readonly SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 readonly SCRIPT_CONFIG="$SCRIPT_DIR/$SCRIPT_NAME.conf"
 readonly SCRIPT_TAG="$(basename "$SCRIPT_PATH")"
 
-HTML_WL="wl0.1 wl0.2 wl0.3 wl1.1 wl1.2 wl1.3 wl2.1 wl2.2 wl2.3 wl3.1 wl3.2 wl3.3" # list of guest network interfaces to generate HTML pages for, separated by space
 ROTATE_WL="wl0.1 wl1.1" # guest network interfaces to randomize passwords for (find them using 'nvram show | grep "^wl[0-9]\.[0-9]_ssid"' command), separated by space
 CHAR_LIST="ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghijklmnpqrstuvwxyz123456789" # characters list for generated passwords
 PASSWORD_LENGTH=20 # length of generated passwords
 ROTATE_ON_START=false # should we rotate passwords on script start
-HTML_FILE="$SCRIPT_DIR/$SCRIPT_NAME.html" # base html file, "#INTERFACE#" string is replaced with interface name
 CRON="0 4 * * *"
 
 if [ -f "$SCRIPT_CONFIG" ]; then
@@ -35,29 +31,6 @@ fi
 get_random_password() {
     #shellcheck disable=SC2002
     cat /dev/urandom | env LC_CTYPE=C tr -dc "$CHAR_LIST" | head -c $PASSWORD_LENGTH; echo;
-}
-
-generate_html_pages() {
-    if [ -f "$HTML_FILE" ]; then
-        LIST_HTML="/www/user/guest-list.html"
-        echo "<ul style=\"font-size:14pt;list-style: none;margin:0;padding:0;\">" > "$LIST_HTML"
-
-        for INTERFACE in $HTML_WL; do
-            SSID="$(nvram get "${INTERFACE}_ssid")"
-
-            if [ -n "$SSID" ]; then
-                cp "$HTML_FILE" "/www/user/guest-$INTERFACE.html"
-                sed -i "s/#INTERFACE#/$INTERFACE/g" "/www/user/guest-$INTERFACE.html"
-
-                echo "<li><a href=\"/user/guest-$INTERFACE.html\">$INTERFACE - $SSID</a></li>" >> "$LIST_HTML"
-            fi
-        done
-        echo "</ul>" >> "$LIST_HTML"
-        
-        logger -st "$SCRIPT_TAG" "Generated HTML pages"
-    else
-        logger -st "$SCRIPT_TAG" "Not generating HTML pages because '$HTML_FILE' does not exist"
-    fi
 }
 
 case "$1" in
@@ -80,11 +53,6 @@ case "$1" in
         done
 
         [ -n "$CHANGED" ] && nvram commit && service restart_wireless
-
-        generate_html_pages
-    ;;
-    "html")
-        generate_html_pages
     ;;
     "start")
         [ -n "$ROTATE_WL" ] && cru a "$SCRIPT_NAME" "$CRON $SCRIPT_PATH run"
@@ -95,8 +63,6 @@ case "$1" in
             else
                 sh "$SCRIPT_PATH" run
             fi
-        else
-            generate_html_pages
         fi
     ;;
     "stop")
@@ -107,7 +73,7 @@ case "$1" in
         sh "$SCRIPT_PATH" start
     ;;
     *)
-        echo "Usage: $0 run|start|stop|restart|html"
+        echo "Usage: $0 run|start|stop|restart"
         exit 1
     ;;
 esac
