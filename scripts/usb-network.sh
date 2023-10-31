0e2a17b6d01fd661ab29b9f90b5521c897657d57
Jack'lul <jacklulcat@gmail.com>
2023-10-31 19:45:52 +0100

    Update lockfile function to utilize flock

--- scripts/usb-network.sh
+++ scripts/usb-network.sh
@@ -25,45 +25,18 @@ if [ -f "$SCRIPT_CONFIG" ]; then
 fi
 
 lockfile() { #LOCKFUNC_START#
-    _LOCKFILE="/tmp/$SCRIPT_NAME.lock"
+    _LOCKFILE="/var/lock/script-$SCRIPT_NAME.lock"
+    _FD=9
 
     case "$1" in
         "lock")
-            if [ -f "$_LOCKFILE" ]; then
-                _LOCKWAITLIMIT=60
-                _LOCKWAITTIMER=0
-                while [ "$_LOCKWAITTIMER" -lt "$_LOCKWAITLIMIT" ]; do
-                    [ ! -f "$_LOCKFILE" ] && break
-
-                    _LOCKPID="$(sed -n '1p' "$_LOCKFILE")"
-                    _LOCKCMD="$(sed -n '2p' "$_LOCKFILE")"
-
-                    [ ! -d "/proc/$_LOCKPID" ] && break;
-                    [ "$_LOCKPID" = "$$" ] && break;
-
-                    _LOCKWAITTIMER=$((_LOCKWAITTIMER+1))
-                    sleep 1
-                done
-
-                [ "$_LOCKWAITTIMER" -ge "$_LOCKWAITLIMIT" ] && { logger -st "$SCRIPT_TAG" "Unable to obtain lock after $_LOCKWAITLIMIT seconds, held by $_LOCKPID ($_LOCKCMD)"; exit 1; }
-            fi
-
-            echo "$$" > "$_LOCKFILE"
-            echo "$@" >> "$_LOCKFILE"
-            trap 'rm -f "$_LOCKFILE"; exit $?' EXIT
+            eval exec "$_FD>$_LOCKFILE"
+            flock -x $_FD
+            trap 'flock -u $_FD; rm -f "$_LOCKFILE"; exit $?' EXIT
         ;;
         "unlock")
-            if [ -f "$_LOCKFILE" ]; then
-                _LOCKPID="$(sed -n '1p' "$_LOCKFILE")"
-
-                if [ -d "/proc/$_LOCKPID" ] && [ "$_LOCKPID" != "$$" ]; then
-                    echo "Attempted to remove not own lock"
-                    exit 1
-                fi
-
-                rm -f "$_LOCKFILE"
-            fi
-
+            flock -u "$_FD"
+            rm -f "$_LOCKFILE"
             trap - EXIT
         ;;
     esac
