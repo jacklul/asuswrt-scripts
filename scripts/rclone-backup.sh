d9cb9fcdde782ac528ad22d54ff94f2af81e1f7b
Jack'lul <jacklulcat@gmail.com>
2023-10-31 20:05:39 +0100

    Automatically install when Entware is available

--- scripts/rclone-backup.sh
+++ scripts/rclone-backup.sh
@@ -35,6 +35,11 @@ if [ -z "$RCLONE_PATH" ] && [ -f "/opt/bin/rclone" ]; then
     RCLONE_PATH="/opt/bin/rclone"
 fi
 
+# Install through Entware
+if [ -z "$RCLONE_PATH" ] && [ -f "/opt/bin/opkg" ]; then
+    /opt/bin/opkg install rclone && RCLONE_PATH="/opt/bin/rclone"
+fi
+
 case "$1" in
     "run")
         { [ "$(nvram get wan0_state_t)" != "2" ] && [ "$(nvram get wan1_state_t)" != "2" ]; } && { echo "WAN network is not connected"; exit 1; }
