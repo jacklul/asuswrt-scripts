0b90100bfe8dd49520c8f34d9d25cd1247985221
Jack'lul <jacklulcat@gmail.com>
2023-11-22 17:51:27 +0100

    Allow modification of temporary www path

--- scripts/modify-webui.sh
+++ scripts/modify-webui.sh
@@ -14,13 +14,12 @@ readonly SCRIPT_CONFIG="$SCRIPT_DIR/$SCRIPT_NAME.conf"
 readonly SCRIPT_TAG="$(basename "$SCRIPT_PATH")"
 
 TWEAKS="cpu_temperature guest_wifi_qr_code notrendmicro_support" # list of tweaks to apply
+TMP_WWW_PATH="/tmp/$SCRIPT_NAME/www"
 
 if [ -f "$SCRIPT_CONFIG" ]; then
     #shellcheck disable=SC1090
     . "$SCRIPT_CONFIG"
 fi
-
-TMP_WWW_PATH="/tmp/$SCRIPT_NAME/www"
 
 [ "$(uname -o)" = "ASUSWRT-Merlin" ] && MERLIN="1"
 
