1ee81dd4b5018ca9fee77432a1eb92808da802f3
Jack'lul <jacklulcat@gmail.com>
2024-04-15 18:25:32 +0200

    Update hotplug-script

--- extras/hotplug-script.sh
+++ extras/hotplug-script.sh
@@ -1,23 +1,15 @@
 #!/bin/sh
 # $2 = subsystem, $3 = action
 # add this script to EXECUTE_COMMAND in hotplug-event.conf
-# to fix Entware in RAM combined with USB network
-#  (github.com/jacklul/asuswrt-usb-raspberry-pi)
+# to fix AsusWRT USB network (github.com/jacklul/asuswrt-usb-raspberry-pi)
+# not starting when using Entware in RAM
+# because /opt is mounted and asusware script can't run
 
 [ -z "$2" ] && exit
 
-case "$1" in
-    "net")
-        FOUND=false
-
-        for INTERFACE in /sys/class/net/usb*; do
-            [ -d "$INTERFACE" ] && FOUND=true && break
-        done
-
-        if [ "$FOUND" = true ]; then
-            /jffs/scripts/entware.sh start
-        else
-            /jffs/scripts/entware.sh stop
-        fi
-    ;;
-esac
+if [ "$1" = "block" ] && [ "$2" = "add" ]; then
+    #shellcheck disable=SC2125
+    ASUSWRT_USB_NETWORK_GLOB=/tmp/mnt/*/asusware*/etc/init.d/S50asuswrt-usb-network
+    #shellcheck disable=SC2086
+    [ -f $ASUSWRT_USB_NETWORK_GLOB ] && sh $ASUSWRT_USB_NETWORK_GLOB start
+fi
