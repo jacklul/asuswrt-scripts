2be79e7ed3292f9720a08617395333dd765148fa
Jack'lul <jacklulcat@gmail.com>
2024-04-16 17:53:35 +0200

    Update lockfile function

--- scripts/wgs-lanonly.sh
+++ scripts/wgs-lanonly.sh
@@ -31,13 +31,15 @@ lockfile() { #LOCKFILE_START#
     _LOCKFILE="/var/lock/script-$SCRIPT_NAME.lock"
     _PIDFILE="/var/run/script-$SCRIPT_NAME.pid"
     _FD=100
+	_FD_MAX=200
 
     if [ -n "$2" ]; then
         _LOCKFILE="/var/lock/script-$SCRIPT_NAME-$2.lock"
         _PIDFILE="/var/run/script-$SCRIPT_NAME-$2.lock"
     fi
 
-    [ -n "$3" ] && [ "$3" -eq "$3" ] && _FD="$3"
+    [ -n "$3" ] && [ "$3" -eq "$3" ] && _FD="$3" && _FD_MAX="$3"
+    [ -n "$4" ] && [ "$4" -eq "$4" ] && _FD_MAX="$4"
 
     [ ! -d /var/lock ] && mkdir -p /var/lock
     [ ! -d /var/run ] && mkdir -p /var/run
@@ -51,7 +53,7 @@ lockfile() { #LOCKFILE_START#
                 echo "File descriptor $_FD is already in use ($(readlink -f "/proc/$$/fd/$_FD"))"
                 _FD=$((_FD+1))
 
-                [ "$_FD" -gt "200" ] && exit 1
+                [ "$_FD" -gt "$_FD_MAX" ] && exit 1
             done
 
             eval exec "$_FD>$_LOCKFILE"
