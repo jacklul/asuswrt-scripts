f4e87215bf87251279658aefc660795b185364a0
Jack'lul <jacklulcat@gmail.com>
2024-05-08 18:59:41 +0200

    Update hotplug-script

--- extras/hotplug-script.sh
+++ extras/hotplug-script.sh
@@ -2,14 +2,27 @@
 # $2 = subsystem, $3 = action
 # add this script to EXECUTE_COMMAND in hotplug-event.conf
 # to fix AsusWRT USB network (github.com/jacklul/asuswrt-usb-raspberry-pi)
-# not starting when also using Entware
-# because /opt is mounted and asusware script can't run
+# not starting when also using Entware because /opt is already mounted
 
 [ -z "$2" ] && exit
 
-if [ "$1" = "block" ] && [ "$2" = "add" ]; then
-    #shellcheck disable=SC2125
-    ASUSWRT_USB_NETWORK_GLOB=/tmp/mnt/*/asusware*/etc/init.d/S50asuswrt-usb-network
-    #shellcheck disable=SC2086
-    [ -f $ASUSWRT_USB_NETWORK_GLOB ] && sh $ASUSWRT_USB_NETWORK_GLOB start
-fi
+case "$2" in
+    "add")
+        case "$1" in
+            "block")
+                #shellcheck disable=SC2125
+                ASUSWRT_USB_NETWORK_GLOB=/tmp/mnt/*/asusware*/etc/init.d/S50asuswrt-usb-network
+                #shellcheck disable=SC2086
+                [ -f $ASUSWRT_USB_NETWORK_GLOB ] && sh $ASUSWRT_USB_NETWORK_GLOB start
+            ;;
+        esac
+    ;;
+    "remove")
+        case "$1" in
+            "block"|"net")
+                # This prevents /rom/.asusrouter->/usr/sbin/app_init_run.sh from copying /opt/etc/init.d/S* to /opt/*.1
+                nvram set apps_mounted_path=/tmp/mnt/invalid
+            ;;
+        esac
+    ;;
+esac
