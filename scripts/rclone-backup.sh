e3c8e032d3f96449f4bf0cf9443f00e92c7970f3
Jack'lul <jacklulcat@gmail.com>
2025-02-28 17:26:07 +0100

    Update rclone-backup

--- scripts/rclone-backup.sh
+++ scripts/rclone-backup.sh
@@ -20,6 +20,8 @@ REMOTE="remote:" # remote to use
 PARAMETERS="--buffer-size 1M --progress --stats 1s --verbose" # optional parameters
 CONFIG_FILE="/jffs/rclone.conf" # Rclone configuration file
 FILTER_FILE="/jffs/rclone.list" # Rclone filter/list file
+SCRIPT_PRE="/jffs/rclone-pre.sh" # execute a command before running rclone command
+SCRIPT_POST="/jffs/rclone-post.sh" # execute a command after running rclone command
 RCLONE_PATH="" # path to Rclone binary
 LOG_FILE="/tmp/rclone.log" # log file
 NVRAMTXT_FILE="/tmp/nvram.txt" # file to dump 'nvram show' result to, empty means don't dump
@@ -61,17 +63,35 @@ case "$1" in
         [ ! -f "$FILTER_FILE" ] && { logger -st "$SCRIPT_TAG" "Could not find filter file: $FILTER_FILE"; exit 1; }
         [ ! -f "$RCLONE_PATH" ] && { logger -st "$SCRIPT_TAG" "Could not find Rclone binary: $RCLONE_PATH"; exit 1; }
 
+        if [ -n "$SCRIPT_PRE" ] && [ -x "$SCRIPT_PRE" ]; then
+            logger -st "$SCRIPT_TAG" "Executing script '$SCRIPT_PRE'..."
+
+            #shellcheck disable=SC1090
+            . "$SCRIPT_PRE"
+        fi
+
         logger -st "$SCRIPT_TAG" "Creating backup..."
 
-        echo "" > "$LOG_FILE"
         [ -n "$NVRAMTXT_FILE" ] && nvram show > "$NVRAMTXT_FILE" 2> /dev/null
         [ -n "$NVRAMCFG_FILE" ] && nvram save "$NVRAMCFG_FILE" 2> /dev/null
 
+        if [ -n "$LOG_FILE" ]; then
+            echo "" > "$LOG_FILE"
+            PARAMETERS="--log-file=$LOG_FILE $PARAMETERS"
+        fi
+
         #shellcheck disable=SC2086
-        "$RCLONE_PATH" sync --config "$CONFIG_FILE" --filter-from="$FILTER_FILE" / "$REMOTE" --log-file="$LOG_FILE" $PARAMETERS
+        "$RCLONE_PATH" sync --config "$CONFIG_FILE" --filter-from="$FILTER_FILE" / "$REMOTE" $PARAMETERS
         STATUS="$?"
 
         rm -f "$NVRAMTXT_FILE" "$NVRAMCFG_FILE"
+
+        if [ -n "$SCRIPT_POST" ] && [ -x "$SCRIPT_POST" ]; then
+            logger -st "$SCRIPT_TAG" "Executing script '$SCRIPT_POST'..."
+
+            #shellcheck disable=SC1090
+            . "$SCRIPT_POST"
+        fi
 
         if [ -n "$ENTWARE_ON_TMPFS" ]; then
             logger -st "$SCRIPT_TAG" "Uninstalling Rclone..."
@@ -84,7 +104,12 @@ case "$1" in
         if [ "$STATUS" = "0" ]; then
             logger -st "$SCRIPT_TAG" "Backup completed successfully"
         else
-            logger -st "$SCRIPT_TAG" "There was an error while running backup, check $LOG_FILE for details"
+            if [ -n "$LOG_FILE" ]; then
+                logger -st "$SCRIPT_TAG" "There was an error while running backup, check '$LOG_FILE' for details"
+            else
+                logger -st "$SCRIPT_TAG" "There was an error while running backup"
+            fi
+
             exit 1
         fi
     ;;
