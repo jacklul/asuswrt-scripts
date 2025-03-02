3716324f275fe406f0749bc9737cb76cebb0ca16
Jack'lul <jacklulcat@gmail.com>
2025-03-02 11:53:29 +0100

    Handle asusware-usbmount too

--- extras/hotplug-event-script.sh
+++ extras/hotplug-event-script.sh
@@ -4,23 +4,26 @@
 # to fix AsusWRT USB network (github.com/jacklul/asuswrt-usb-raspberry-pi)
 # not starting when also using Entware because /opt is already mounted
 
+#shellcheck disable=SC2125,SC2086
+
 [ -z "$2" ] && exit
 
 case "$2" in
     "add")
         case "$1" in
             "block")
-                #shellcheck disable=SC2125
                 asuswrt_usb_network=/tmp/mnt/*/asusware*/etc/init.d/S50asuswrt-usb-network
-                #shellcheck disable=SC2086
                 [ -f $asuswrt_usb_network ] && sh $asuswrt_usb_network start
+                usb_mount_script=/tmp/mnt/*/asusware*/etc/init.d/S50usb-mount-script
+                [ -f $usb_mount_script ] && sh $usb_mount_script start
             ;;
         esac
     ;;
     "remove")
         case "$1" in
-            "block"|"net")
+            "block")
                 # This prevents /rom/.asusrouter->/usr/sbin/app_init_run.sh from copying /opt/etc/init.d/S* to /opt/*.1
+                # In most cases it will stop the Asus apps from starting, which is what we want at this point
                 nvram set apps_mounted_path=/tmp/mnt/invalid
             ;;
         esac
