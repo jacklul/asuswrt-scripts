d1564fa9ea2a6ff2f38ff8798dc413a789ad983e
Jack'lul <jacklulcat@gmail.com>
2025-08-24 20:44:03 +0200

    Comment formatting, remove permissions.sh as useless

--- asusware-usbmount/asusware.arm/permissions.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cdir="$(dirname "$(readlink -f "$0")")"
-find "$cdir" -type d -exec chmod 755 {} +
-find "$cdir" -type f -exec chmod 644 {} +
-chmod +x "$0" "$cdir/etc/init.d/S50usb-mount-script"
